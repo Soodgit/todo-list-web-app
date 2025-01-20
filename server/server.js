74b18bd545cd6e3532cf2cbbfd3b25070ee7a8ab
Nishant55Sood <nishant.sud55@gmail.com>
2025-01-20 13:30:25 +0530

    fix issue

--- server/server.js
+++ server/server.js
@@ -1,18 +1,13 @@
-const express = require ('express');
+const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const connectDB = require('./config/db');
 const todoRoutes = require('./routes/todoRoutes');
 const errorHandler = require('./middleware/errorHandler');
 
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 const API_URL = process.env.API_URL;
-
-
-
-
 
 // Connect to database
 connectDB();
@@ -21,15 +16,20 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
+// Add CSP headers middleware
+app.use((req, res, next) => {
+  res.setHeader(
+    'Content-Security-Policy',
+    "font-src 'self' data: https://todo-list-web-app-3odh.onrender.com; default-src 'self';"
+  );
+  next();
+});
+
 // Routes
 app.use(`/api/todos`, todoRoutes);
 
 // Error Handler
 app.use(errorHandler);
-
-
-
-
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
