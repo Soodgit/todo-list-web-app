3a52454ba29da0871d6357e6ca96ca50f569e0b3
Nishant55Sood <nishant.sud55@gmail.com>
2025-01-20 13:33:36 +0530

    fix issue

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const connectDB = require('./config/db');
 const todoRoutes = require('./routes/todoRoutes');
 const errorHandler = require('./middleware/errorHandler');
+const path = require('path'); // Add this line
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -16,17 +17,30 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-// Add CSP headers middleware
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// CSP Headers - Modified to be more permissive
 app.use((req, res, next) => {
   res.setHeader(
     'Content-Security-Policy',
-    "font-src 'self' data: https://todo-list-web-app-3odh.onrender.com; default-src 'self';"
+    "default-src 'self'; " +
+    "font-src 'self' data: *; " +
+    "style-src 'self' 'unsafe-inline'; " +
+    "script-src 'self' 'unsafe-inline' 'unsafe-eval'; " +
+    "img-src 'self' data: https:; " +
+    "connect-src 'self' *;"
   );
   next();
 });
 
 // Routes
 app.use(`/api/todos`, todoRoutes);
+
+// Serve your frontend
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // Error Handler
 app.use(errorHandler);
