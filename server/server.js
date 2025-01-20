c8c1cada4e71f22a79fa0b7a0e3df474afc775f9
Nishant55Sood <nishant.sud55@gmail.com>
2025-01-20 12:08:07 +0530

    fix issue

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const errorHandler = require('./middleware/errorHandler');
 
 
 const app = express();
-//const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 const API_URL = process.env.API_URL;
 
 // Connect to database
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/api/todos', todoRoutes);
+app.use(`${API_URL}/api/todos`, todoRoutes);
 
 // Error Handler
 app.use(errorHandler);
